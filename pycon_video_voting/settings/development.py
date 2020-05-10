18ccdb57833e662f03a42463276bf206f311d1e6
Glen Jarvis <glen@glenjarvis.com>
2020-05-10 13:42:38 -0700

    Add STATIC_ROOT to meet Heroku deployment criteria

--- pycon_video_voting/settings/development.py
+++ pycon_video_voting/settings/development.py
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = './static_root/'   ## TODO: Consider removing when we add white noise. Let's see if Heroku actually needs it.
